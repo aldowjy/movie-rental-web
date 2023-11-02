e0ec67f590dfe0939fe8dce25148b5bfb9ae0f48
Aldo Wijaya <35536105+aldowjy@users.noreply.github.com>
2023-11-03 01:30:07 +0700

    add dummy data for movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Button,
+  Center,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  Wrap,
+  Container,
+  Image,
+  Divider,
+  Box,
+} from "@chakra-ui/react";
+import { MdShoppingCart, MdSearch } from "react-icons/md";
+import { MOVIE_DATA } from "./dummy";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container maxW="container.xl" color="#262626">
+        <Center>
+          <Stack spacing={8} direction="row">
+            <Input
+              placeholder="Please Input Movie Name..."
+              size="lg"
+              htmlSize={40}
+              width="auto"
+            />
+            <Button leftIcon={<MdSearch />} colorScheme="teal" size="lg">
+              Find
+            </Button>
+          </Stack>
+        </Center>
+
+        <Wrap mt={8} spacing={4}>
+          {MOVIE_DATA.results.map((data, i) => {
+            return (
+              <Card key={i} maxW="xs">
+                <CardBody>
+                  <Image
+                    src={data.movieImage}
+                    alt={data.title}
+                    borderRadius="lg"
+                  />
+                  <Stack mt="6" spacing="3">
+                    <Heading size="md" align="left">
+                      {data.title}
+                    </Heading>
+                    <Box as="h4" lineHeight="tight" noOfLines={4} align="left">
+                      <Text>{data.opening_crawl}</Text>
+                    </Box>
+                    <Text align="left" color="blue.600" fontSize="xl">
+                      {data.price?.toLocaleString()}
+                    </Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <Button leftIcon={<MdShoppingCart />} colorScheme="blue">
+                    Add to cart
+                  </Button>
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </Wrap>
+      </Container>
     </>
   );
 }
