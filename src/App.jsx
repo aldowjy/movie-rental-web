5b41ab61dd7b655499ae2f2387db989ee55c885e
Aldo Wijaya <aldowjy12@gmail.com>
2023-11-05 23:25:17 +0700

    add image assets and fix app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,19 @@
 import "./App.css";
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Button,
-  Center,
-  Heading,
-  Input,
-  Stack,
-  Text,
-  Wrap,
-  Container,
-  Image,
-  Divider,
-  Box,
-} from "@chakra-ui/react";
-import { MdShoppingCart, MdSearch } from "react-icons/md";
-import { MOVIE_DATA } from "./dummy";
+import { Container } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import MovieList from "./pages/MovieList";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <>
-      <Container maxW="container.xl" color="#262626">
-        <Center>
-          <Stack spacing={8} direction="row">
-            <Input
-              placeholder="Please Input Movie Name..."
-              size="lg"
-              htmlSize={40}
-              width="auto"
-            />
-            <Button leftIcon={<MdSearch />} colorScheme="teal" size="lg">
-              Find
-            </Button>
-          </Stack>
-        </Center>
-
-        <Wrap mt={8} spacing={4}>
-          {MOVIE_DATA.results.map((data, i) => {
-            return (
-              <Card key={i} maxW="xs">
-                <CardBody>
-                  <Image
-                    src={data.movieImage}
-                    alt={data.title}
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md" align="left">
-                      {data.title}
-                    </Heading>
-                    <Box as="h4" lineHeight="tight" noOfLines={4} align="left">
-                      <Text>{data.opening_crawl}</Text>
-                    </Box>
-                    <Text align="left" color="blue.600" fontSize="xl">
-                      {data.price?.toLocaleString()}
-                    </Text>
-                  </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                  <Button leftIcon={<MdShoppingCart />} colorScheme="blue">
-                    Add to cart
-                  </Button>
-                </CardFooter>
-              </Card>
-            );
-          })}
-        </Wrap>
+      <Header />
+      <Container maxW="container.xl">
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/detail/:movieName" element={<MovieDetail />} />
+        </Routes>
       </Container>
     </>
   );
---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+export { default as a_new_hope } from "./a_new_hope.jpg";
+export { default as attack_of_the_clones } from "./attack_of_the_clones.jpg";
+export { default as return_of_the_jedi } from "./return_of_the_jedi.jpg";
+export { default as revenge_of_the_sith } from "./revenge_of_the_sith.jpg";
+export { default as the_empire_strikes_back } from "./the_empire_strikes_back.jpg";
+export { default as the_phantom_menace } from "./the_phantom_menace.jpg";
